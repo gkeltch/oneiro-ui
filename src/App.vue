394922451dc7ac2cab039daeedafe2206097868a
EC3 <Gracie@apotomelabs.com>
2025-04-21 16:41:18 -0600

    homepage design

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,65 @@ import HelloWorld from './components/HelloWorld.vue'
 import Card from "@/components/Card.vue";
 import CardReverse from "@/components/CardReverse.vue";
 import data from "@/data/data.ts";
+import MenuIcon from "@/components/icons/MenuIcon.vue";
+import SearchIcon from "@/components/icons/SearchIcon.vue";
 </script>
 
 <template>
 
-  <div class="title">
-    Oneiro
-  </div>
+  <div class="app">
 
-  <div class="navbar">
-    <div>home</div>
-    <div>log in</div>
-    <div>about</div>
+    <div class="titleBar">
+      <div class="">
+        SOMNA
+      </div>
 
-  </div>
-  <div class="contentGrid">
+      <div class="navbar">
+        <div>home</div>
+        <div>log in</div>
+        <div>about</div>
+      </div>
 
-    <component
-      v-for="(card, index) in data.about"
-      :key="index"
-      :is="index % 2 === 0 ? Card : CardReverse"
-      :name="card.name"
-      :desc="card.desc"
-      :image="card.image"
-    />
+      <div class="buttons">
+        <SearchIcon/>
+        <MenuIcon/>
+      </div>
+    </div>
+
+      <div class="contentWrap">
+
+        <div class="navLeft">
+          <div class="socials">
+            <div>Fb</div>
+            <div>In</div>
+            <div>Tw</div>
+          </div>
+        </div>
+
+        <div class="contentGrid">
+
+          <div class="infoWrap">
+            <div class="info">
+              <div class="bottom">{{data.aboutDescLong}}</div>
+            </div>
+          </div>
+
+          <div class="imageWrap">
+            <div class="title">SOMNA</div>
+            <div class="image"/>
+          </div>
+
+          <div class="infoWrap">
+            <div class="info right">
+              <div>{{data.aboutDescLong2}}</div>
+              <div class="button bottom">SIGN UP</div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
 
   </div>
 
@@ -36,14 +71,109 @@ import data from "@/data/data.ts";
 
 <style scoped lang="scss">
 
-@import "assets/Colors";
-@import "assets/Text";
+@import "assets/Library";
+
+
+.titleBar {
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid $light;
+  padding: 0 1vw;
+}
+
+.right{
+  text-align: right;
+}
+.button{
+  margin-left:auto;
+}
+.app{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+}
+
+.imageWrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  grid-column: 2;
+  position: relative;
+}
+
+.image{
+  background-image: url("/dream4.jpg");
+  background-size: cover;
+  background-position: 100% 50%;
+  width: 100%;
+  height: 80%;
+  justify-self: center;
+}
+
+.infoWrap{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  flex-direction: column;
+}
+
+.info{
+  padding: 48px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+}
+
+.bottom{
+  margin-top: auto;
+}
+
+.infoWrap2{
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+
+.info2{
+  padding: 48px;
+  height: 80%;
+}
+
+.contentWrap{
+  display: flex;
+  height: 100%;
+}
+
+.navLeft{
+  border: 2px solid $light;
+  border-top: none;
+  width: 48px;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+
+.socials{
+  border-top: 2px solid $light;
+  display: flex;
+  flex-direction: column;
+  padding: 1vw;
+  justify-content: center;
+  align-items: center;
+}
 
 .title{
-  color:$light;
-  font-size:$fontBig;
+  position: absolute;
+  font-size: $fontBig;
+  letter-spacing: 6vw;
   text-align: center;
-  font-family: "Bungee Shade", sans-serif;
+  justify-self: center;
+  align-self: center;
+  margin-right: -6vw;
 }
 
 .name{
@@ -52,14 +182,15 @@ import data from "@/data/data.ts";
 
 .navbar{
   display: flex;
-  width: 50%;
   justify-content: space-evenly;
   justify-self: center;
+  width: 60%;
 }
 
 .contentGrid{
+  width: 100%;
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: repeat(3,1fr);
 }
 
 
--- src/data/data.ts
+++ src/data/data.ts
@@ -16,8 +16,10 @@ const data = {
       desc:"Dreams can be a powerful reflection of our waking emotions. By consistently journaling and reflecting on your dreams, you may gain a deeper understanding of complex feelings, anxieties, and joys that you might not be fully aware of in your daily life. Use your dream journal as a tool for self-discovery and emotional growth.",
       image:"/dream3.jpg"
     }
-  ]
-
+  ],
+  aboutDescShort:"Unlock the world within your dreams with Somna. Your simple, private digital journal to easily capture, reflect on, and discover patterns in your nightly adventures.",
+  aboutDescLong:"Dive into the fascinating landscape of your subconscious with Somna, your dedicated dream journal. Dreams offer a unique window into our minds, but they fade quickly. Somna provides an easy, private space to record your experiences before they vanish. Track recurring themes, moods, and symbols over time, and begin to uncover the hidden patterns and insights waiting within your dreams. Start your journey of self-discovery, one night at a time.",
+  aboutDescLong2: "Your dreams are a hidden world, rich with symbols, stories, and emotions. But how often do these intricate narratives slip away just moments after waking? Somna is designed to be your trusted companion on the journey into this personal, inner landscape.",
 }
 
 export default data;
